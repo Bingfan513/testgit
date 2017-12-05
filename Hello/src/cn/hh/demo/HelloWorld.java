637941abf8510c8c7592bb37ea05c8dea6ef0ad0
Bingfan513 <Bingfan513@163.com>
2017-12-05 17:45:26 +0800

    改正

--- Hello/src/cn/hh/demo/HelloWorld.java
+++ Hello/src/cn/hh/demo/HelloWorld.java
@@ -3,5 +3,13 @@ package cn.hh.demo;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello£¬world£¡");
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println("*");
+			for (int j = 10; j > 0; j--) {
+				System.out.println("#");
+			}
+		}
 	}
+	
 }
